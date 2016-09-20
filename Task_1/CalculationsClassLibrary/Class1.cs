4e34eb086d142ac3af9d7a09880d93811c547ef5
Uladzislau Lesneuski <Uladzislau_Lesneuski@epam.com>
2016-09-20 15:17:40 +0300

    add first task

---
+++ Task_1/CalculationsClassLibrary/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculationsClassLibrary
+{
+    public class Calculations
+    {
+        public int addition(int number1, int number2)
+        {
+            int sum = 0;
+            sum = number1 + number2;
+            return sum;
+        }
+
+        public int subtraction(int number1, int number2)
+        {
+            int difference = 0;
+            difference = number1 - number2;
+            return difference;
+        }
+
+        public int multiplication(int number1, int number2)
+        {
+            int composition = 0;
+            composition = number1 * number2;
+            return composition;
+        }
+
+        public int division(int number1, int number2)
+        {
+            int quotient = 0;
+            quotient = number1 / number2;
+            return quotient;
+        }
+    }
+}
---
+++ Task_1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CalculationsClassLibrary;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = Int32.Parse(Console.ReadLine());
+            int number2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(number1 + number2);
+
+            Calculations calc = new Calculations();
+
+            Console.WriteLine(calc.addition(number1, number2));
+            Console.WriteLine(calc.subtraction(number1,number2));
+            Console.WriteLine(calc.multiplication(number1, number2));
+            Console.WriteLine(calc.division(number1, number2));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
